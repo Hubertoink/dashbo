fa9ae807cfaffa0523429be1cb9ac28593a04648
Hubertoink <hubertoink@outlook.com>
2026-01-08 23:18:24 +0100

    UI_Improv

--- frontend/src/lib/components/WeekPlannerTodoBar.svelte
+++ frontend/src/lib/components/WeekPlannerTodoBar.svelte
@@ -271,8 +271,8 @@
       aria-label="ToDo hinzufügen"
       title="ToDo hinzufügen"
       on:click={toggleExpand}
-      in:fade={{ duration: 150, delay: 150 }}
-      out:fade={{ duration: 100 }}
+      in:fade={{ duration: 120, delay: 180 }}
+      out:fade={{ duration: 80 }}
     >
       <svg class="w-5 h-5 text-white/80" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
